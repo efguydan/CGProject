c1e53bee1bff596160a94389605129efd9e0a0b9
EfeDaniel <EfeEjemudaro@gmail.com>
2019-12-25 00:32:47 +0100

    Feature: Add instructions for key controlled cube

--- app/src/main/java/com/r4sh33d/cgproject/cube/CubeDetailsFragment.java
+++ app/src/main/java/com/r4sh33d/cgproject/cube/CubeDetailsFragment.java
@@ -2,6 +2,7 @@ package com.r4sh33d.cgproject.cube;
 
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -12,6 +13,7 @@ import android.view.ViewGroup;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
+import com.afollestad.materialdialogs.MaterialDialog;
 import com.r4sh33d.cgproject.R;
 import com.r4sh33d.cgproject.fragments.BaseFragment;
 
@@ -50,6 +52,19 @@ public class CubeDetailsFragment extends BaseFragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         setToolbarTitle("GL Cubes");
+        showCubeDetails();
+    }
+
+    private void showCubeDetails() {
+        MaterialDialog materialDialog = new MaterialDialog.Builder(getContext())
+                .title("GL Cubes")
+                .content(getString(R.string.cube_details))
+                .positiveText("Okay")
+                .onPositive((dialog, which) -> dialog.dismiss()).build();
+        materialDialog.getContentView().setTextSize(18);
+        materialDialog.getContentView().setTextColor(Color.BLACK);
+        materialDialog.getTitleView().setTextAppearance(getContext(), android.R.style.TextAppearance_Material_Headline);
+        materialDialog.show();
     }
 
     @OnClick(R.id.cube_draw)
@@ -59,6 +74,22 @@ public class CubeDetailsFragment extends BaseFragment {
         RadioButton cubeMovementButton = getView().findViewById(movementRadioGroup.getCheckedRadioButtonId());
         String movementType = cubeMovementButton.getText().toString();
 
+        if (movementType.equals("Key Controlled Cube")) {
+            MaterialDialog materialDialog = new MaterialDialog.Builder(getContext())
+                    .title("GL Cubes")
+                    .content(getString(R.string.key_controlled_interaction_details))
+                    .positiveText("Proceed")
+                    .onPositive((dialog, which) -> moveToRenderer(cubeType, movementType)).build();
+            materialDialog.getContentView().setTextSize(18);
+            materialDialog.getContentView().setTextColor(Color.BLACK);
+            materialDialog.getTitleView().setTextAppearance(getContext(), android.R.style.TextAppearance_Material_Headline);
+            materialDialog.show();
+        } else {
+            moveToRenderer(cubeType, movementType);
+        }
+    }
+
+    private void moveToRenderer(String cubeType, String movementType) {
         Intent intent = new Intent(getContext(), CubeActivity.class);
         intent.putExtra(CubeActivity.CUBE_TYPE, cubeType);
         intent.putExtra(CubeActivity.MOVEMENT_TYPE, movementType);
